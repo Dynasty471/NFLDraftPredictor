cb3c3335ccf5d65b1753a81e24cf68cd28a1d9b1
Tianran Yuan <tianran@royal-09.cs.wisc.edu>
2017-02-17 13:56:20 -0600

    Change from NFL stats to college stats

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,18 +3,19 @@ from bs4 import BeautifulSoup
 
 BASE_URL = 'http://www.pro-football-reference.com'
 
+
 def parse_player(link):
-	r = requests.get(link)
-	rsoup = BeautifulSoup(r.text, 'html.parser')
+    r = requests.get(link)
+    rsoup = BeautifulSoup(r.text, 'html.parser')
     stats = {}
     get_measurements(rsoup, stats)
 
-	seasons = rsoup.find_all('tr')	
-	for season in seasons:
-		stats = season.find_all('td')
-		for stat in stats:
-			print(stat.get('data-stat'))
-			print(stat.string)		
+    seasons = rsoup.find_all('tr')	
+    for season in seasons:
+        stats = season.find_all('td')
+        for stat in stats:
+            print(stat.get('data-stat'))
+            print(stat.string)		
 
 def get_measurements(soup, stats):
     for span_tag in soup.find_all('span'):
@@ -26,15 +27,21 @@ def get_measurements(soup, stats):
             print("Weight: " + span_tag.string)
             stats['Weight'] = span_tag.string
 
+# Go through the draft table, and pull the player college stats page and
+# the draft position.
 def generate_dataset():
     r = requests.get('http://www.pro-football-reference.com/years/2015/draft.htm')
     rsoup = BeautifulSoup(r.text, 'html.parser')
+    # Isolate each individual player row.
     player_rows = rsoup.find_all('tr')
     for player in player_rows:
-        if player.strong:
-            link = player.strong.a.get('href')
-            link = BASE_URL + link
-            parse_player(link)
+        # Go through each column of each row.
+        columns = player.find_all('td')
+        for column in columns:
+            if column.get('data-stat') == "college_link":
+                link = player.strong.a.get('href')
+                link = BASE_URL + link
+                parse_player(link)
 
 if __name__ == "__main__":
     generate_dataset()
